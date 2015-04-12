aebcc83910c23073a1421297fb7283f6f00a7ba8
redbeardster <redbeardster@gmail.com>
2015-04-12 23:07:12 +0500

    multicast registration add

--- src/udpsrv.erl
+++ src/udpsrv.erl
@@ -3,6 +3,9 @@
 -behaviour(gen_server).
  
 -define(SERVER, ?MODULE).
+-define(Addr, {224,0,0,251}).
+
+
  
 -export([start_link/0]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
@@ -17,8 +20,10 @@ init([]) ->
 %    {ok, #state{}}.
 
     Port = 5000,
-    {ok, Socket} = gen_udp:open(Port),
+    {ok, Socket} = gen_udp:open(Port,[{reuseaddr,true}, {ip,?Addr}, {multicast_ttl,4}, {multicast_loop,false}, binary]),
+    inet:setopts(Socket,[{add_membership,{{?Addr,{0,0,0,0}}}}]),
     error_logger:info_msg("Listen in port ~p~n", [Port]),
+
     {ok, #state{socket=Socket}}.
  
 
@@ -32,6 +37,13 @@ handle_cast(_Msg, State) ->
  
 handle_info({udp,_Socket,{_A,_B,_C,_D},_Port,String}, State) ->
     error_logger:info_msg("Received via INFO: ~p~n", [String]),
+    case net_adm:ping(erlang:binary_to_atom(String, latin1)) of
+	pong -> 
+	    io:format("~w~n", nodes());
+	_ ->
+	    io:format("Oops!~n")
+	end,
+
     {noreply, State}.
  
 terminate(_Reason, State) ->
