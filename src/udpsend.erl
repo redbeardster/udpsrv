54b5d761a006fb292181c78700494b592a3d2a59
redbeardster <redbeardster@gmail.com>
2015-04-12 23:13:33 +0500

    sender added

---
+++ src/udpsend.erl
@@ -0,0 +1,13 @@
+-module(udpsend).
+-export([start/0]).
+-define (Addr, {224,0,0,251}).
+-define (Myaddr, {10,10,174,141}).
+-define (IAddr, {0,0,0,0}).
+-define (Port, 5000).
+
+start() ->
+
+{ok, Socket} = gen_udp:open(Port=5000, [binary, {active, false}, {reuseaddr, true},
+                                        {ip, ?Addr}, {add_membership, {?Addr, ?IAddr}}]),
+    gen_udp:send(Socket,?Addr,5000,erlang:atom_to_list(node())),
+    gen_udp:close(Socket).
